ff59bdf2531dc80d659c9bc77384edee1209e8f4
hxbaishikele <1543087373@qq.com>
2017-12-31 10:29:21 +0800

    tabbar

--- web-roadhog/src/router.js
+++ web-roadhog/src/router.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Router, Route } from 'dva/router';
 import IndexPage from './routes/IndexPage';
+import TabBarExample from './routes/TabBarExample';
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
-      <Route path="/" component={IndexPage} />
+      <Route path="/" component={TabBarExample} />
     </Router>
   );
 }
