97da508fa567c478d639fa0fba4ad27ab51be1d6
hxbaishikele <1543087373@qq.com>
2017-12-31 19:16:40 +0800

    echarts

--- web-roadhog/src/routes/FirstTab/FirstTab.js
+++ web-roadhog/src/routes/FirstTab/FirstTab.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { connect } from 'dva';
-
+import SimpleChartComponent from '../chart/Echarts/SimpleChartComponent';
 const FirstTab =({count,dispatch}) => {
   const {msg} = count;
   return (
     <div>
-    <div>hahgag</div>
+      <div>hahgag</div>
       <button onClick={() =>{dispatch({type:'count/query'});}}>我不知道</button>
       <div>{msg}</div>
+      <SimpleChartComponent />
     </div>
   );
 }
